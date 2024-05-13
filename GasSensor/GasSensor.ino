4c2be6c6d1f3574a889505b1c0fbd94552afaa29
Rajeev MYLAPALLI <rajeev.msn@gmail.com>
2024-05-13 18:32:32 +0200

    Arduino code update

--- GasSensor/GasSensor.ino
+++ GasSensor/GasSensor.ino
@@ -1,5 +1,20 @@
+/*
+This code is to send MQ135 output to Serial Monitor
+----------
+Board: Arduino UNO
+=========================
+Refer to MQ135 datasheet and examples
+==========================
+---------------
+Version: 2.0
+Updated on 13 May 2024
+by Nicolas Traut, Roberto Toro & Rajeev Mylapalli
+*/ 
+
 int sensorValue;
 int digitalValue;
+float slope = 1.0;    // Adjust the slope based on calibration
+int intercept = 0;    // Adjust the intercept based on calibration
 
 void setup()
 {
@@ -12,13 +27,20 @@ void loop()
 {
   sensorValue = analogRead(0); // read analog input pin 0
   digitalValue = digitalRead(2);
-  if (sensorValue > 400)
+
+  // You can adjust the slope and intercept based on calibration
+  float gasConcentration = (sensorValue - intercept) / slope;
+
+  if (gasConcentration > 400)
   {
     digitalWrite(13, HIGH);
   }
   else
     digitalWrite(13, LOW);
-  Serial.println(sensorValue, DEC);
-  //Serial.println(digitalValue, DEC);
+
+  
+  Serial.print("<data>");
+  Serial.print(sensorValue, DEC);
+  Serial.println("</data>");
   delay(10000); 
 }
